5ee76ab3452c4fed3963cd4a3e82bfaf55264bd1
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-05-21 23:14:11 +0600

    all-plant-sort-added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -18,38 +18,57 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const plantCollection = client.db('platnDB').collection('plants');
+    const plantCollection = client.db("platnDB").collection("plants");
 
-    app.get('/plants',async(req,res)=>{
-      const result = await plantCollection.find().toArray();
-      res.send(result);
-    })
+    app.get("/plants", async (req, res) => {
+      const sortBy = req.query.sortBy;
 
-    app.get('/plants/:id',async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
-      const result = await plantCollection.findOne(query);
-      res.send(result)
-    })
+      if (sortBy === "nextWatering") {
+        const result = await plantCollection
+          .aggregate([
+            {
+              $addFields: {
+                nextWateringDate: { $toDate: "$nextWatering" },
+              },
+            },
+            {
+              $sort: { nextWateringDate: 1 },
+            },
+          ])
+          .toArray();
 
-    app.post('/plants',async(req,res)=>{
-        const plant = req.body;
-        const result = await plantCollection.insertOne(plant);
         res.send(result);
-    })
+      } else {
+        const result = await plantCollection.find().toArray();
+        res.send(result);
+      }
+    });
 
+    app.get("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.post("/plants", async (req, res) => {
+      const plant = req.body;
+      const result = await plantCollection.insertOne(plant);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -57,12 +76,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
-app.get('/',(req,res)=>{
-    res.send('server is running')
-})
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-    
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
